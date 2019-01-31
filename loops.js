f879f2d924c8ce1e9103926d1959fe05767c591c
Daniel Bailey <daniel4423@gmail.com>
2019-01-31 20:24:14 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -12,11 +12,22 @@ function whileLoop(n) {
   return 'done';
 }
 
+//my doWhileLoop doesn't need this code
+//It doesn't seem to have any effect, &
+//the test passes with or without it
+//but the lab instructions say to use it
+var i = 0;
 
+function incrementVariable() {
+  i = i + 1;
+}
 
 function doWhileLoop(array) {
   do {
     array.pop()
-  } while (array.length > 0)
+  } while (array.length > 0 && incrementVariable())
+  //Again, it doesn't matter if I incude the && incrementVariabl() or not.
+  //The test passes either way.
+  //Not sure why it was suggested to use it.
   return array;
 }
